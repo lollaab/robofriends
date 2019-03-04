ee744f316548db1a2d34c41ba7149a9ea73e14ea
lolla_ab <naela_ab@yahoo.com>
2019-03-04 13:56:27 +0300

    commit

---
+++ build/precache-manifest.2f3c70ea4414c9fc0ef51aa0075a8474.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "4c8fc31ee6029a568ca7",
+    "url": "/static/js/main.4c8fc31e.chunk.js"
+  },
+  {
+    "revision": "e8a72efa66a54e0bab48",
+    "url": "/static/js/2.e8a72efa.chunk.js"
+  },
+  {
+    "revision": "4c8fc31ee6029a568ca7",
+    "url": "/static/css/main.4aaf3947.chunk.css"
+  },
+  {
+    "revision": "e8a72efa66a54e0bab48",
+    "url": "/static/css/2.1fb47d77.chunk.css"
+  },
+  {
+    "revision": "9e1ee11323496f3c8ff80726491cbfdc",
+    "url": "/index.html"
+  }
+];
