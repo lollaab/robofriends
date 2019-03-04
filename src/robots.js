a93a07da248b5950e971f5e1f8c1a58b4aec9ed5
lolla_ab <naela_ab@yahoo.com>
2019-03-04 14:15:01 +0300

    commit

---
+++ src/robots.js
@@ -0,0 +1,49 @@
+export const robots =[
+{
+	id: 1,
+	name: 'Lane Graham',
+	email: 'Leana Graham.com'
+
+},
+{
+	id: 2,
+	name: 'Ervin Howel',
+	email: 'Ervin.com'
+
+},
+{
+	id: 3,
+	name: 'Claema',
+	email: 'Claema.com'
+
+},
+{
+	id: 4,
+	name: 'Sotea',
+	email: 'Sotea.com'
+
+},
+{
+	id: 5,
+	name: 'Particia',
+	email: 'Particia.com'
+
+}
+,
+{
+	id: 6,
+	name: 'Particia',
+	email: 'Particia.com'
+
+},
+{
+	id: 7,
+	name: 'Particia',
+	email: 'Particia.com'
+
+}
+]
+
+
+
+
