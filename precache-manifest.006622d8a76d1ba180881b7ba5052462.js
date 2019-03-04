0150ffe4adf20551219499c14ce3cff7399b5518
lolla_ab <naela_ab@yahoo.com>
2019-03-04 14:26:20 +0300

    Updates

---
+++ precache-manifest.006622d8a76d1ba180881b7ba5052462.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "aedb4e53e89ac656e8a6",
+    "url": "/robofriends/static/js/runtime~main.aedb4e53.js"
+  },
+  {
+    "revision": "35576dac448cd89ca496",
+    "url": "/robofriends/static/js/main.35576dac.chunk.js"
+  },
+  {
+    "revision": "e8a72efa66a54e0bab48",
+    "url": "/robofriends/static/js/2.e8a72efa.chunk.js"
+  },
+  {
+    "revision": "35576dac448cd89ca496",
+    "url": "/robofriends/static/css/main.66eab5d1.chunk.css"
+  },
+  {
+    "revision": "e8a72efa66a54e0bab48",
+    "url": "/robofriends/static/css/2.1fb47d77.chunk.css"
+  },
+  {
+    "revision": "3970b2098ae87462f7b4bc3932f58564",
+    "url": "/robofriends/index.html"
+  }
+];
